71e3d2a5099ab2087185976c8143b16e2602fe7f
HydraNhani <hydra.qntp1764@gmail.com>
2021-04-11 13:24:48 +0200

    Switched to beta token

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import 'module-alias/register';
 import dotenv from 'dotenv';
-import "discord-reply"; 
+import 'discord-reply'; 
 import GalaxyAlpha from '@root/Client';
 
 //ENVIROMENT VARIABLES\\
@@ -9,15 +9,15 @@ dotenv.config();
 const token: string = process.env.TOKEN;
 const betaToken: string = process.env.BETA_TOKEN;
 export default new GalaxyAlpha({
-    ownerID: "700277649347575870",
+    ownerID: '700277649347575870',
     globalPrefix: process.env.GLOBAL_PREFIX,
-    token: token,
-    commandsDir: "Commands",
-    eventsDir: "Events",
-    featuresDir: "Features",
+    token: betaToken,
+    commandsDir: 'Commands',
+    eventsDir: 'Events',
+    featuresDir: 'Features',
     mongoDBUrl: process.env.MONGOPATH,
     contributors: ['694162762020159539', '358303166845943808'],
-    supportGuildID: "783440776285651024",
-    ignoreFiles: ["Giveaway.ts", "Music.ts", "Drop.ts", "Ticket.ts", "Constants.ts", "Queue.ts", "BrawlerData.ts"],
-    slashCommandsDir: "SlashCommands"
+    supportGuildID: '783440776285651024',
+    ignoreFiles: ['Giveaway.ts', 'Music.ts', 'Drop.ts', 'Ticket.ts', 'Constants.ts', 'Queue.ts', 'BrawlerData.ts'],
+    slashCommandsDir: 'SlashCommands'
 });
